f0d84864ce1f37a9886f4584067fcbb75ff25ae5
MrFosta <57017969+MrFosta@users.noreply.github.com>
2019-11-03 20:32:15 -0500

    added enemies in rooms

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,11 @@ public class GameManager : Singleton<GameManager>
     public Room[,] rooms;
     public int mapSizeX, mapSizeY;
     public List<Vector2> takenPositions = new List<Vector2>();
-    public int floorNumber = 4;
+    public int floorNumber = 1;
+    public int enemiesAlive = 0;
+    public GameObject[] enemyTypes;
+    public int[,] clearedRooms;
+    
 
     // Handle to the player
     private GameObject player;
@@ -46,23 +50,76 @@ public class GameManager : Singleton<GameManager>
                 tiles[a, b] = -1;
             }
         }
-        tileTypes = new TileType[2];
+        clearedRooms = new int[mapSizeX, mapSizeY];
+        enemyTypes = new GameObject[1];
+        enemyTypes[0] = Resources.Load<GameObject>("Enemy");
+
+        tileTypes = new TileType[10];
 
         //player = Resources.Load<GameObject>("player");
 
-        //i was getting a null reference exception
-        TileType yeet1 = new TileType();
-        TileType yeet2 = new TileType();
+        
+        TileType tt1 = new TileType();
+        TileType tt2 = new TileType();
+        TileType tt3 = new TileType();
+        TileType tt4 = new TileType();
+        TileType tt5 = new TileType();
+        TileType tt6 = new TileType();
+        TileType tt7 = new TileType();
+        TileType tt8 = new TileType();
+        TileType tt9 = new TileType();
+        TileType tt10 = new TileType();
 
-        yeet1.name = "FloorTile";
-        yeet1.tileVisualPrefab = Resources.Load<GameObject>("FloorTile");
-        yeet1.movementCost = 1;
-        yeet1.isWalkable = true;
-        tileTypes[0] = yeet1;
-        yeet2.name = "WallTile";
-        yeet2.tileVisualPrefab = Resources.Load<GameObject>("WallTile");
-        yeet2.movementCost = 1;
-        yeet2.isWalkable = false;
-        tileTypes[1] = yeet2;
+        tt1.name = "FloorTile";
+        tt1.tileVisualPrefab = Resources.Load<GameObject>("FloorTile");
+        tt1.movementCost = 1;
+        tt1.isWalkable = true;
+        tileTypes[0] = tt1;
+        tt2.name = "WallTile";
+        tt2.tileVisualPrefab = Resources.Load<GameObject>("WallTile");
+        tt2.movementCost = 1;
+        tt2.isWalkable = false;
+        tileTypes[1] = tt2;
+        tt3.name = "doorTopClosed";
+        tt3.tileVisualPrefab = Resources.Load<GameObject>("doorTopClosed");
+        tt3.movementCost = 1;
+        tt3.isWalkable = false;
+        tileTypes[2] = tt3;
+        tt4.name = "doorTopOpen";
+        tt4.tileVisualPrefab = Resources.Load<GameObject>("doorTopOpen");
+        tt4.movementCost = 1;
+        tt4.isWalkable = true;
+        tileTypes[3] = tt4;
+        tt5.name = "doorLeftClosed";
+        tt5.tileVisualPrefab = Resources.Load<GameObject>("doorLeftClosed");
+        tt5.movementCost = 1;
+        tt5.isWalkable = false;
+        tileTypes[4] = tt5;
+        tt6.name = "doorLeftOpen";
+        tt6.tileVisualPrefab = Resources.Load<GameObject>("doorLeftOpen");
+        tt6.movementCost = 1;
+        tt6.isWalkable = true;
+        tileTypes[5] = tt6;
+        tt7.name = "doorRightClosed";
+        tt7.tileVisualPrefab = Resources.Load<GameObject>("doorRightClosed");
+        tt7.movementCost = 1;
+        tt7.isWalkable = false;
+        tileTypes[6] = tt7;
+        tt8.name = "doorRightOpen";
+        tt8.tileVisualPrefab = Resources.Load<GameObject>("doorRightOpen");
+        tt8.movementCost = 1;
+        tt8.isWalkable = true;
+        tileTypes[7] = tt8;
+        tt9.name = "doorBottomClosed";
+        tt9.tileVisualPrefab = Resources.Load<GameObject>("doorBottomClosed");
+        tt9.movementCost = 1;
+        tt9.isWalkable = false;
+        tileTypes[8] = tt9;
+        tt10.name = "doorBottomOpen";
+        tt10.tileVisualPrefab = Resources.Load<GameObject>("doorBottomOpen");
+        tt10.movementCost = 1;
+        tt10.isWalkable = true;
+        tileTypes[9] = tt10;
+
     }
 }
---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+
+    [SerializeField] Image image;
+    private Item _item;
+    public Item Item {
+      get {return _item;}
+      set {
+        _item = value;
+        if (_item == null) {
+          image.enabled = false;
+        } else {
+          image.sprite = _item.Icon;
+          image.enabled = true;
+        }
+      }
+    }
+
+    private void OnValidate() {
+      image = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
